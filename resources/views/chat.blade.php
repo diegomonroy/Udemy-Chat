035de443371aba60af0b0d7c111288a0219dd023
Diego Fernando Monroy Muñoz <dmsoftwaresas@gmail.com>
2020-04-05 00:08:51 +0000

    Setting Up Project.

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+		<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+	</head>
+	<body>
+		<div class="container">
+			<div id="app" class="row">
+				<ul class="list-group offset-4 col-4">
+					<li class="list-group-item active">Chat Room</li>
+					<li class="list-group-item">Dapibus ac facilisis in</li>
+					<li class="list-group-item">Morbi leo risus</li>
+					<li class="list-group-item">Porta ac consectetur ac</li>
+					<li class="list-group-item">Vestibulum at eros</li>
+					<input type="text" class="form-control" placeholder="Type your message here...">
+				</ul>
+			</div>
+		</div>
+		<script src="{{ asset('js/app.js') }}"></script>
+	</body>
+</html>
